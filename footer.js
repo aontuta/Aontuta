26fb72049ebf3da9c89f05ba2fed0c259b5b9b77
aontuta <31282368+aontuta@users.noreply.github.com>
2023-07-21 11:33:07 +0700

    Update footer.js

--- footer.js
+++ footer.js
@@ -1,3 +1,3 @@
 function setFooter(){
-  document.querySelector('.copyright').innerHTML = '<div class="text-center"><a href="https://www.facebook.com/groups/exambloggas" target="_blank"></a></div><center style="font-size:12px;">Examblog &copy; 2023 </center>'
+ document.querySelector('.copyright').innerHTML = '<div class="text-center"><a href="https://www.facebook.com/groups/exambloggas" target="_blank"></a></div><center style="font-size:12px;">Examblog &copy; 2023 </center>'
   }

b75897ab19480f89fc44156e0d3e0c083b70726a
aontuta <31282368+aontuta@users.noreply.github.com>
2023-07-21 11:17:27 +0700

    Update footer.js

--- footer.js
+++ footer.js
@@ -1,3 +1,3 @@
 function setFooter(){
-  document.querySelector('.copyright').innerHTML = '<div class="text-center"></div><center style="font-size:12px;">Examblog &copy; 2023 <a style="color:gray;text-decoration:none" href="https://examblog64.krooluang.com/p/examvip.html" target="_blank"> Contact</a></center>'
+  document.querySelector('.copyright').innerHTML = '<div class="text-center"></div><center style="font-size:12px;">Examblog &copy; 2023 </center>'
   }

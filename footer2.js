1ab7675c9a9d740c8c6a49cc52647a00eb8f344f
aontuta <31282368+aontuta@users.noreply.github.com>
2023-07-21 11:29:12 +0700

    Create footer2.js

---
+++ footer2.js
@@ -0,0 +1,1 @@
+ document.querySelector('.copyright').innerHTML = '<div class="text-center"><a href="https://www.facebook.com/groups/exambloggas" target="_blank"><i class="fa-brands fa-facebook fa-2x"></i></a></div><center style="font-size:12px;">Examblog &copy; 2023 </center>'
